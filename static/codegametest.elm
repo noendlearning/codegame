8a7a206f9904db9fbce132aeebf57f4e4b7291eb
LuminousZYT <48241053+LuminousZYT@users.noreply.github.com>
2019-04-12 11:33:23 +0800

    页面测试样式elm

---
+++ static/codegametest.elm
@@ -0,0 +1,180 @@
+module Main exposing (main, view)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+type Msg
+    = Display
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Display ->
+            0
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ class "all" ]
+        [ div
+            [ class "nag" ]
+            [ a
+                [ href "#" ]
+                [ img
+                    [ src "static/images/head.png" ]
+                    []
+                ]
+            , a
+                [ href "#" ]
+                [ img
+                    [ src "static/images/level.png" ]
+                    []
+                ]
+            , a
+                [ href "#" ]
+                [ img
+                    [ src "static/images/btn1.png" ]
+                    []
+                ]
+            , a
+                [ href "#" ]
+                [ img
+                    [ src "static/images/btn2.png" ]
+                    []
+                ]
+            , a
+                [ href "#" ]
+                [ img
+                    [ src "static/images/btn3.png" ]
+                    []
+                ]
+            , a
+                [ href "#" ]
+                [ img
+                    [ src "static/images/btn4.png" ]
+                    []
+                ]
+            , div
+                [ class "friends" ]
+                [ a
+                    [ href "#" ]
+                    [ img
+                        [ src "static/images/friends.png" ]
+                        []
+                    ]
+                ]
+            ]
+        , div
+            [ class "chat" ]
+            []
+        , div
+            [ class "mains" ]
+            [ div
+                [ class "banner" ]
+                [ div
+                    [ class "word_one" ]
+                    [ text "ASCII Art" ]
+                ]
+            , div
+                [ class "containers" ]
+                [ div
+                    [ class "left" ]
+                    [ div
+                        [ class "discription" ]
+                        []
+                    , div
+                        [ class "console_output" ]
+                        [ div
+                            [ class "output" ]
+                            [ text "Console output" ]
+                        , div
+                            [ class "put" ]
+                            []
+                        ]
+                    ]
+                , div
+                    [ class "right" ]
+                    [ div
+                        [ class "write_code" ]
+                        [ div
+                            [ class "write_code" ]
+                            [ div
+                                [ class "write_top" ]
+                                [ select
+                                    [ class "drop-down" ]
+                                    [ option
+                                        []
+                                        [ text "Elm" ]
+                                    , option
+                                        []
+                                        [ text "Haskell" ]
+                                    , option
+                                        []
+                                        [ text "Java" ]
+                                    , option
+                                        []
+                                        [ text "Python" ]
+                                    , option
+                                        []
+                                        [ text "PHP" ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    , div
+                        [ class "right_bottom" ]
+                        [ div
+                            [ class "test_cases" ]
+                            [ div
+                                [ class "top" ]
+                                [ div
+                                    [ class "word_two" ]
+                                    [ text "Test cases" ]
+                                , div
+                                    [ class "img_one" ]
+                                    [ img
+                                        [ src "static/images/menu.png" ]
+                                        []
+                                    ]
+                                ]
+                            , div
+                                [ class "bottom" ]
+                                []
+                            ]
+                        , div
+                            [ class "actions" ]
+                            [ div
+                                [ class "actions_top" ]
+                                [ text "Action" ]
+                            , div
+                                [ class "actions_bottom" ]
+                                [ button
+                                    [ class "btn_1" ]
+                                    [ text "▶ PLAY ALL   TESTCASES" ]
+                                , button
+                                    [ class "btn_2" ]
+                                    [ text "✔ SUBMIT" ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
