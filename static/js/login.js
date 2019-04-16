7d306d7cf9944bf4f0884310212c9984b925cbd0
朱玉婷 <48241053+LuminousZYT@users.noreply.github.com>
2019-04-16 17:51:05 +0800

    Email格式基本验证待完善

--- static/js/login.js
+++ static/js/login.js
@@ -79,3 +79,21 @@ $(function ($) {
     $("#mask").css({ display: 'none' });
   });
 });
+
+//验证邮箱
+function check(){
+  //alert("1234756");
+　　var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$"); //正则表达式
+　　var obj = document.getElementById("txtName"); //要验证的对象
+　　if(obj.value == ""){ //输入不能为空
+　　　　alert("输入不能为空!");
+　　　　return false;
+　　}else if(!reg.test(obj.value)){ //正则验证不通过，格式不对
+　　　　alert("验证不通过!");
+        obj.value="";
+　　　　return false;
+　　}else{
+　　　　alert("通过！");
+　　　　return true;
+　　}
+}
