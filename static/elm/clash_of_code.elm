fadbe45b7b30a4263ebf6ae411019b77c9259f32
chengwanqiang <859871734@qq.com>
2019-04-28 09:07:22 +0800

    完成json数据传递的功能

--- static/elm/clash_of_code.elm
+++ static/elm/clash_of_code.elm
@@ -1,17 +1,13 @@
 module Main exposing (main)
 
--- import Css exposing (..)
-
 import Array exposing (..)
 import Browser
-import Debug
+import Debug exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
-
-
-
--- import Html.Styled.Attributes exposing (..)
+import Http exposing (..)
+import Json.Encode as Encode
 
 
 main =
@@ -23,24 +19,191 @@ main =
         }
 
 
-type alias Model =
+type alias Title =
     String
+
+
+type alias Statement =
+    String
+
+
+type alias InputDescription =
+    String
+
+
+type alias OutputDescription =
+    String
+
+
+type alias Constraints =
+    String
+
+
+type alias TestCases_Test =
+    { input : String, output : String }
+
+
+type alias TestCases_Validater =
+    { input : String, output : String }
+
+
+type alias TestCases =
+    { testName : String
+    , validator : String
+    , test : TestCases_Test
+    , validater : TestCases_Validater
+    }
+
+
+type alias Model =
+    { title : Title --标题
+    , statement : Statement --声明
+    , inputDescription : InputDescription --输入说明
+    , outputDescription : OutputDescription --输出说明
+    , constraints : Constraints --限制条件
+    , testCases : TestCases --测试用例
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( "ceshi", Cmd.none )
+    ( { title = ""
+      , statement = ""
+      , inputDescription = ""
+      , outputDescription = ""
+      , constraints = ""
+      , testCases =
+            { testName = "test1"
+            , validator = "validator1"
+            , test =
+                { input = ""
+                , output = ""
+                }
+            , validater =
+                { input = ""
+                , output = ""
+                }
+            }
+      }
+    , Cmd.none
+    )
 
 
 type Msg
-    = Display
+    = Display (Result Http.Error String)
+    | PushDate
+    | ChangeTitle String
+    | ChangeStatemen String
+    | ChangeInputDescription String
+    | ChangeOutputDescription String
+    | ChangeConstraints String
+    | ChangeTestCasesTestName String
+    | ChangeTestCasesValidator String
+    | ChangeTestCasesTestInput String
+    | ChangeTestCasesTestOutput String
+    | ChangeTestCasesValidaterInput String
+    | ChangeTestCasesValidaterOutput String
+    | ChangeTestCases TestCases
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Display ->
+        Display result ->
+            case result of
+                Ok fullt ->
+                    ( model, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+        PushDate ->
+            Debug.log "ceshi"
+                ( model
+                , Http.post
+                    { url = "/code"
+                    , body =
+                        Http.jsonBody
+                            (Encode.object
+                                [ ( "title", Encode.string model.title )
+                                , ( "statement", Encode.string model.statement )
+                                , ( "inputDescription", Encode.string model.inputDescription )
+                                , ( "outputDescription", Encode.string model.outputDescription )
+                                , ( "constraints", Encode.string model.constraints )
+                                , ( "testCases"
+                                  , Encode.object
+                                        [ ( "testName", Encode.string model.testCases.testName )
+                                        , ( "validator", Encode.string model.testCases.validator )
+                                        , ( "test"
+                                          , Encode.object
+                                                [ ( "input", Encode.string model.testCases.test.input )
+                                                , ( "output", Encode.string model.testCases.test.output )
+                                                ]
+                                          )
+                                        , ( "validater"
+                                          , Encode.object
+                                                [ ( "input", Encode.string model.testCases.validater.input )
+                                                , ( "output", Encode.string model.testCases.validater.output )
+                                                ]
+                                          )
+                                        ]
+                                  )
+                                ]
+                            )
+                    , expect = Http.expectString Display
+                    }
+                )
+
+        ChangeTitle newTitle ->
+            ( { model | title = newTitle }, Cmd.none )
+
+        ChangeStatemen newStatement ->
+            ( { model | statement = newStatement }, Cmd.none )
+
+        ChangeInputDescription newInputDescription ->
+            ( { model | inputDescription = newInputDescription }, Cmd.none )
+
+        ChangeOutputDescription newOutputDescription ->
+            ( { model | outputDescription = newOutputDescription }, Cmd.none )
+
+        ChangeConstraints newConstraints ->
+            ( { model | constraints = newConstraints }, Cmd.none )
+
+        ChangeTestCasesTestName newTestCasesTestName ->
+            updateTestName (\testCases -> { testCases | testName = newTestCasesTestName }) model
+
+        ChangeTestCasesValidator newTestCasesValidator ->
+            updateTestName (\testCases -> { testCases | validator = newTestCasesValidator }) model
+
+        ChangeTestCasesTestInput newTestCasesTestInput ->
+            updateTestCasesTestInput (\test -> { test | input = newTestCasesTestInput }) model.testCases model
+
+        ChangeTestCasesTestOutput newTestCasesTestOutput ->
+            updateTestCasesTestInput (\test -> { test | output = newTestCasesTestOutput }) model.testCases model
+
+        ChangeTestCasesValidaterInput newTestCasesValidaterInput ->
+            updateTestCasesValidater (\validater -> { validater | input = newTestCasesValidaterInput }) model.testCases model
+
+        ChangeTestCasesValidaterOutput newTestCasesValidaterOutput ->
+            updateTestCasesValidater (\validater -> { validater | output = newTestCasesValidaterOutput }) model.testCases model
+
+        ChangeTestCases newTestCases ->
             ( model, Cmd.none )
+
+
+updateTestName : (TestCases -> TestCases) -> Model -> ( Model, Cmd Msg )
+updateTestName transform model =
+    ( { model | testCases = transform model.testCases }, Cmd.none )
+
+
+updateTestCasesTestInput : (TestCases_Test -> TestCases_Test) -> TestCases -> Model -> ( Model, Cmd Msg )
+updateTestCasesTestInput transdate testCases model =
+    ( { model | testCases = { testCases | test = transdate model.testCases.test } }, Cmd.none )
+
+
+updateTestCasesValidater : (TestCases_Validater -> TestCases_Validater) -> TestCases -> Model -> ( Model, Cmd Msg )
+updateTestCasesValidater transdate testCases model =
+    ( { model | testCases = { testCases | validater = transdate model.testCases.validater } }, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -82,6 +245,8 @@ view model =
                     [ style "border" "1px solid #bbbbbb"
                     , style "width" "100%"
                     , style "height" "40px"
+                    , value model.title
+                    , onInput ChangeTitle
                     ]
                     []
                 , p [] [ text "声明" ]
@@ -89,6 +254,8 @@ view model =
                     [ style "border" "1px solid #bbbbbb"
                     , style "width" "100%"
                     , style "height" "80px"
+                    , value model.statement
+                    , onInput ChangeStatemen
                     ]
                     []
                 , p [] [ text "输入说明" ]
@@ -96,6 +263,8 @@ view model =
                     [ style "border" "1px solid #bbbbbb"
                     , style "width" "100%"
                     , style "height" "80px"
+                    , value model.inputDescription
+                    , onInput ChangeInputDescription
                     ]
                     []
                 , p [] [ text "输出说明" ]
@@ -103,6 +272,8 @@ view model =
                     [ style "border" "1px solid #bbbbbb"
                     , style "width" "100%"
                     , style "height" "80px"
+                    , value model.outputDescription
+                    , onInput ChangeOutputDescription
                     ]
                     []
                 , p [] [ text "限制条件" ]
@@ -110,6 +281,8 @@ view model =
                     [ style "border" "1px solid #bbbbbb"
                     , style "width" "100%"
                     , style "height" "80px"
+                    , value model.constraints
+                    , onInput ChangeConstraints
                     ]
                     []
                 , div
@@ -129,32 +302,40 @@ view model =
                     [ text "测试用例"
                     , p [ style "color" "#838891" ] [ text "测试1" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
+                        , value model.testCases.test.input
+                        , onInput ChangeTestCasesTestInput
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
+                        , value model.testCases.test.output
+                        , onInput ChangeTestCasesTestOutput
                         ]
-                        [ text "输出" ]
+                        []
                     , p [ style "color" "#838891" ] [ text "验证器1" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
+                        , value model.testCases.validater.input
+                        , onInput ChangeTestCasesValidaterInput
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
+                        , value model.testCases.validater.output
+                        , onInput ChangeTestCasesValidaterOutput
                         ]
-                        [ text "输出" ]
+                        []
                     , div
                         --分割线
                         [ style "border" "1px solid #bbbbbb"
@@ -163,32 +344,32 @@ view model =
                         []
                     , p [ style "color" "#838891" ] [ text "测试2" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
                         ]
-                        [ text "输出" ]
+                        []
                     , p [ style "color" "#838891" ] [ text "验证器2" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
                         ]
-                        [ text "输出" ]
+                        []
                     , div
                         --分割线
                         [ style "border" "1px solid #bbbbbb"
@@ -197,32 +378,32 @@ view model =
                         []
                     , p [ style "color" "#838891" ] [ text "测试3" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
                         ]
-                        [ text "输出" ]
+                        []
                     , p [ style "color" "#838891" ] [ text "验证3" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
                         ]
-                        [ text "输出" ]
+                        []
                     , div
                         --分割线
                         [ style "border" "1px solid #bbbbbb"
@@ -231,32 +412,32 @@ view model =
                         []
                     , p [ style "color" "#838891" ] [ text "测试4" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
                         ]
-                        [ text "输出" ]
+                        []
                     , p [ style "color" "#838891" ] [ text "验证4" ]
                     , textarea
-                        [ style "placeholder" "输入"
+                        [ placeholder "输入"
                         , style "width" "45%"
                         , style "height" "80px"
                         ]
-                        [ text "输入" ]
+                        []
                     , textarea
-                        [ style "placeholder" "输出"
+                        [ placeholder "输出"
                         , style "width" "45%"
                         , style "height" "80px"
                         , style "margin-left" "20px"
                         ]
-                        [ text "输出" ]
+                        []
                     , button
                         [ style "width" "150px"
                         , style "height" "30px"
@@ -329,14 +510,17 @@ view model =
                             , text "草案（他不会被其他编辑器看到）"
                             ]
                         ]
-                    , button
-                        [ style "width" "50%"
+                    , div
+                        [ style "display" "inline-block"
+                        , style "width" "50%"
                         , style "height" "50px"
                         , style "background-color" "rgba(242,187,19,0.7)"
+                        , onClick PushDate
                         ]
                         [ text "保存" ]
-                    , button
-                        [ style "width" "50%"
+                    , div
+                        [ style "display" "inline-block"
+                        , style "width" "50%"
                         , style "height" "50px"
                         , style "background-color" "rgba(69,76,85,0.1)"
                         ]
