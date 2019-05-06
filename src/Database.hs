1ad9808ec501487966613d3e7cffd864a51f0967
Atrongant <402635876@qq.com>
2019-05-06 15:37:18 +0800

    加密用户密码

--- src/Database.hs
+++ src/Database.hs
@@ -7,30 +7,42 @@
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
+
 -- {-# LANGUAGE PackageImports #-}
-
 module Database
-    ( testFunc,selectByUUID,selectByEmail,updateEmailByUUID,updatePwdByUUID,insertUser
-    ) where
+  ( testFunc
+  , selectByUUID
+  , selectByEmail
+  , updateEmailByUUID
+  , updatePwdByUUID
+  , insertUser
+  , getStrictPwd
+  ) where
 
-import           Control.Monad.IO.Class  (liftIO)
+import           Control.Monad.IO.Class              (liftIO)
 import           Control.Monad.Trans.Reader
-import Database.Persist 
-import  Database.Persist.MySQL 
-import    Database.Persist.TH
--- import  "persistent"         Database.Persist 
--- import "persistent-mysql" Database.Persist.MySQL 
+import           Database.Persist
+import           Database.Persist.MySQL
+import           Database.Persist.TH
+import Database.Persist.Class (PersistField(toPersistValue))
+import           Control.Monad.IO.Class
+-- import  "persistent"         Database.Persist
+-- import "persistent-mysql" Database.Persist.MySQL
 -- import    "persistent-template"       Database.Persist.TH
 import           Control.Monad.Logger
-import           Database.Esqueleto as E
-import qualified  Data.UUID.V4 as UV (nextRandom)
-import qualified Data.UUID  as DU (toString,UUID)
-import Debug.Trace
-import Control.Monad.Trans.Reader
-import Database.Persist.Sql.Types.Internal
-import Control.Monad.IO.Class
+import           Control.Monad.Trans.Reader
+import qualified Data.UUID                           as DU (UUID, toString)
+import qualified Data.UUID.V4                        as UV (nextRandom)
+import           Database.Esqueleto                  as E
+import           Database.Persist.Sql.Types.Internal
+import           Debug.Trace
+import Data.Text (unpack,pack)
+import Data.Password.Instances
+import Data.Text.Internal
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
 User
     uuid String
     email String
@@ -40,10 +52,9 @@ User
 
 testFunc :: IO ()
 testFunc = do
-    uuid1 <- UV.nextRandom
-    -- runStdoutLoggingT . withMySQLPool conInfo 10 . runSqlPool $ do
-    runNoLoggingT . withMySQLPool conInfo 10 . runSqlPool $ do
-        runMigration migrateAll
+  uuid1 <- UV.nextRandom
+  runNoLoggingT . withMySQLPool conInfo 10 . runSqlPool $ do
+    runMigration migrateAll
         -- johnId <- insert $ User (DU.toString uuid1) "123@qq.com" "123"
         -- johnId <- insert $ User (DU.toString uuid2) "234@qq.com" "234"
         -- johnId <- insert $ User (DU.toString uuid3) "345@qq.com" "345"
@@ -55,58 +66,108 @@ testFunc = do
         --             return p
         -- updateEmailByUUID "fe95c189-5720-49d6-9059-8c94dd373fd2" "402635876@qq.com"
         -- updatePwdByUUID "fe95c189-5720-49d6-9059-8c94dd373fd2" "5mayiwen"
-        -- people<-selectByUUID "fe95c189-5720-49d6-9059-8c94dd373fd2"
-        -- traceM(show(people))  
-        -- [Entity {entityKey = UserKey {unUserKey = SqlBackendKey {unSqlBackendKey = 7}}, 
-        -- entityVal = User {userUuid = "fe95c189-5720-49d6-9059-8c94dd373fd2", userEmail = "123@qq.com", userPassword = "123"}}]          
+        -- traceM(show(people))
+        -- [Entity {entityKey = UserKey {unUserKey = SqlBackendKey {unSqlBackendKey = 7}},
+        -- entityVal = User {userUuid = "fe95c189-5720-49d6-9059-8c94dd373fd2", userEmail = "123@qq.com", userPassword = "123"}}]
         -- liftIO $ mapM_ (putStrLn . userEmail . entityVal) people
-        insertUser "godev@gmail.com" "jbandmyw" uuid1
-        people <- E.select $ E.from $ \user -> return user
-        liftIO $ mapM_ (putStrLn . userEmail . entityVal) people
+    -- let pwd =Pass "5mayiwen"
+    -- let salt = Salt "hnbrina2019"
+    -- let hashedPassword=hashPassWithSalt pwd salt
+    -- let strictPwd=unpack $ unPassHash hashedPassword
+    insertUser "godev1" "5mayiwen" uuid1
+    people <- E.select $ E.from $ \user -> return user
+    liftIO $ mapM_ (putStrLn . userEmail . entityVal) people
+
 -- 插入用户
-insertUser:: (MonadIO m,PersistStoreWrite backend,BaseBackend backend ~ SqlBackend) =>String-> String-> DU.UUID -> ReaderT backend m ()
-insertUser email pwd uuid=
-    insert_ $ User (DU.toString uuid) email pwd
+insertUser ::
+     (MonadIO m, PersistStoreWrite backend, BaseBackend backend ~ SqlBackend)
+  => String
+  -> String
+  -> DU.UUID
+  -> ReaderT backend m ()
+insertUser email pwd uuid = insert_ $ User (DU.toString uuid) email $ getStrictPwd pwd
+
+originalsalt = "hnbrina2019XN9dUU8uhnbrina2019bQSkvEZIRhnbrina2019UWr9UVWCjzOLsU=hnbrina2019LbmItlhltyIHhnbrina20194Nro2YyMFeCCKwtV0=hnbrina2019"
+
+getStrictPwd :: String -> String
+getStrictPwd password=
+        let
+            pwd =Pass $ pack password
+            salt = Salt originalsalt
+            hashedPassword=hashPassWithSalt pwd salt
+        in
+            unpack $ unPassHash hashedPassword
 
 -- 根据uuid查询用户
-selectByUUID :: (MonadIO m,BackendCompatible SqlBackend backend,PersistQueryRead  backend,PersistUniqueRead  backend) =>[Char]-> ReaderT backend m [Entity User]
-selectByUUID uuid=  E.select $
-    E.from $ \p -> do
+selectByUUID ::
+     ( MonadIO m
+     , BackendCompatible SqlBackend backend
+     , PersistQueryRead backend
+     , PersistUniqueRead backend
+     )
+  => [Char]
+  -> ReaderT backend m [Entity User]
+selectByUUID uuid =
+  E.select $
+  E.from $ \p -> do
     E.where_ (p ^. UserUuid E.==. val uuid)
     return p
 
 -- select via email
-selectByEmail :: (MonadIO m,BackendCompatible SqlBackend backend,PersistQueryRead  backend,PersistUniqueRead  backend) =>[Char]-> ReaderT backend m [Entity User]
-selectByEmail email=
-        E.select $
-        E.from $ \p -> do
-        E.where_ (p ^. UserEmail E.==. val email)
-        return p
-
+selectByEmail ::
+     ( MonadIO m
+     , BackendCompatible SqlBackend backend
+     , PersistQueryRead backend
+     , PersistUniqueRead backend
+     )
+  => [Char]
+  -> ReaderT backend m [Entity User]
+selectByEmail email =
+  E.select $
+  E.from $ \p -> do
+    E.where_ (p ^. UserEmail E.==. val email)
+    return p
 
 -- update email  via uuid
-updateEmailByUUID :: (MonadIO m,BackendCompatible SqlBackend backend,PersistQueryWrite backend,PersistUniqueWrite backend) =>[Char] -> [Char] -> ReaderT backend m ()
-updateEmailByUUID uuid email=
-        E.update $ \p -> do
-        E.set p [ UserEmail E.=. val email ]
-        E.where_ (p ^. UserUuid E.==. val uuid)
+updateEmailByUUID ::
+     ( MonadIO m
+     , BackendCompatible SqlBackend backend
+     , PersistQueryWrite backend
+     , PersistUniqueWrite backend
+     )
+  => [Char]
+  -> [Char]
+  -> ReaderT backend m ()
+updateEmailByUUID uuid email =
+  E.update $ \p -> do
+    E.set p [UserEmail E.=. val email]
+    E.where_ (p ^. UserUuid E.==. val uuid)
 
 --update pwd by uuid
-updatePwdByUUID :: (MonadIO m,BackendCompatible SqlBackend backend,PersistQueryWrite backend,PersistUniqueWrite backend) =>[Char] -> [Char] -> ReaderT backend m ()
-updatePwdByUUID uuid pwd=
-    E.update $ \p -> do
-    E.set p [ UserPassword E.=. val pwd ]
+updatePwdByUUID ::
+     ( MonadIO m
+     , BackendCompatible SqlBackend backend
+     , PersistQueryWrite backend
+     , PersistUniqueWrite backend
+     )
+  => [Char]
+  -> [Char]
+  -> ReaderT backend m ()
+updatePwdByUUID uuid pwd =
+  E.update $ \p -> do
+    E.set p [UserPassword E.=. val pwd]
     E.where_ (p ^. UserUuid E.==. val uuid)
 
 -- mysql 数据库连接
 conInfo :: ConnectInfo
-conInfo =ConnectInfo {
-    connectHost="localhost",
-    connectPort=3306,
-    connectUser="root",
-    connectPassword="1",
-    connectDatabase="test",
-    connectOptions=[],
-    connectPath="",
-    connectSSL=Nothing
-}
+conInfo =
+  ConnectInfo
+    { connectHost = "localhost"
+    , connectPort = 3306
+    , connectUser = "root"
+    , connectPassword = "1"
+    , connectDatabase = "test"
+    , connectOptions = []
+    , connectPath = ""
+    , connectSSL = Nothing
+    }
