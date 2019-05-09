7195b3f040e86a71a4caa288c497ff54bda27bca
马义雯 <402635876@qq.com>
2019-05-09 11:40:39 +0800

    新增工具文件夹

---
+++ src/Tool/Types.hs
@@ -0,0 +1,8 @@
+module Tool.Types where
+
+import ClassyPrelude
+import Database.Persist.TH
+
+-- data MyState = Employed | Unemployed | Retired
+--     deriving (Show, Read, Eq)
+-- derivePersistField "Employment"   
