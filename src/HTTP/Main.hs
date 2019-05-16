03ccfd3626a2af8c7c03cee67c43cc0c1e6fbd3f
马义雯 <402635876@qq.com>
2019-05-16 17:40:59 +0800

    静态不需要cookie也可以访问

--- src/HTTP/Main.hs
+++ src/HTTP/Main.hs
@@ -1,62 +1,70 @@
 module HTTP.Main where
 
 
-import ClassyPrelude
-
-import Network.Wai
-import Network.Wai.Handler.Warp
-import System.IO.Unsafe (unsafePerformIO)
-import qualified HTTP.API.Auth as Api
-import Network.HTTP.Types (status200, unauthorized401, status404)
-
-main ::Int -> IO ()
-main port = do
-  putStrLn "open port:"
-  print port
-  run port app
-
-   
-app :: Application
-app req respond =  do
-  res <- Api.hasCookieInfo req
-  traceM(show(res))
-  case res of
-    Nothing->
-      -- fixme: 转发到index请求
-      respond $ resFile "text/html" "static/index.html"  
-    Just a->  
-      respond $ 
+  import ClassyPrelude
+  
+  import Network.Wai
+  import Network.Wai.Handler.Warp
+  import System.IO.Unsafe (unsafePerformIO)
+  import qualified HTTP.API.Auth as Api
+  import Network.HTTP.Types (status200, unauthorized401, status404)
+  
+  main ::Int -> IO ()
+  main port = do
+    putStrLn "open port:"
+    print port
+    run port app
+  
+     
+  app :: Application
+  app req respond =  do
+    res <- Api.hasCookieInfo req
+    traceM(show(res))
+    case res of
+      Nothing->
+        respond $ 
         case pathInfo req of
-          ["loginUser"] -> 
-            unsafePerformIO $ Api.loginUser req 
-          ["registerUser"] -> 
-            unsafePerformIO $ Api.registerUser req   
-          ["play"] -> 
-                -- unsafePerformIO 函数是取出IO中的 Response
-                unsafePerformIO $ Api.testParam req   
-          ["init"] -> 
-            unsafePerformIO $ Api.initCode req 
+        -- fixme: 转发到index请求
           ["static", subDir, fileName] -> 
-                serveStatic subDir fileName  
-          [] -> 
+            serveStatic subDir fileName  
+          _-> 
             resFile "text/html" "static/index.html"  
-          ["index"] -> 
-            resFile "text/html" "static/index.html"  
-          _ -> res404      
-
-
-resFile :: ByteString -> FilePath -> Response
-resFile contentType filename = responseFile status200 [("Content-Type", contentType)] filename Nothing    
-
-serveStatic :: Text -> Text -> Response
-serveStatic subDir fName = 
-  case sub of
-    "js" -> serve "text/javascript"
-    "css" -> serve "text/css"
-    "images" -> serve "image/png"
-    _ -> res404
-  where serve mimeType = resFile mimeType $ concat ["static/", sub, "/", unpack fName]
-        sub = unpack subDir
-
-res404 :: Response
-res404 = responseLBS status404 [] $ fromString "Not Found"                
+          -- ["index"] -> 
+          --   resFile "text/html" "static/index.html" 
+          -- respond $ resFile "text/html" "static/index.html"  
+      Just a->  
+        respond $ 
+          case pathInfo req of
+            ["loginUser"] -> 
+              unsafePerformIO $ Api.loginUser req 
+            ["registerUser"] -> 
+              unsafePerformIO $ Api.registerUser req   
+            ["play"] -> 
+                  -- unsafePerformIO 函数是取出IO中的 Response
+                  unsafePerformIO $ Api.testParam req   
+            ["init"] -> 
+              unsafePerformIO $ Api.initCode req 
+            ["static", subDir, fileName] -> 
+                  serveStatic subDir fileName  
+            [] -> 
+              resFile "text/html" "static/index.html"  
+            ["index"] -> 
+              resFile "text/html" "static/index.html"  
+            _ -> res404      
+  
+  
+  resFile :: ByteString -> FilePath -> Response
+  resFile contentType filename = responseFile status200 [("Content-Type", contentType)] filename Nothing    
+  
+  serveStatic :: Text -> Text -> Response
+  serveStatic subDir fName = 
+    case sub of
+      "js" -> serve "text/javascript"
+      "css" -> serve "text/css"
+      "images" -> serve "image/png"
+      _ -> res404
+    where serve mimeType = resFile mimeType $ concat ["static/", sub, "/", unpack fName]
+          sub = unpack subDir
+  
+  res404 :: Response
+  res404 = responseLBS status404 [] $ fromString "Not Found"                
