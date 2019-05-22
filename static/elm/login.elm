2a154e1fea4ea447b22334061c81520ecef58e92
马义雯 <402635876@qq.com>
2019-05-22 15:37:04 +0800

    修改首页

--- static/elm/login.elm
+++ static/elm/login.elm
@@ -3,18 +3,29 @@ module Main exposing (main, view)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
-import Http
+import Html.Events exposing (onClick,onInput)
+import Http exposing (..)
 
+
+-- main =
+--     Browser.sandbox { init = init, update = update, view = view }
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 type alias Model =
     { loadState : StateModel
-    , login : Login
-    , singup : Singup
+    , form : Form
+    -- , singup : Form
     , emailState : Int
     , pwdState : Int
     }
@@ -22,35 +33,26 @@ type alias Model =
 -- 1 填写正确
 -- 2 填写错误
 
-type alias Login =
+type alias Form =
     { email : String
     , password : String
     }
-type alias Singup =
-    { email : String
-    , password : String
-    }
+-- type alias Singup =
+--     { email : String
+--     , password : String
+--     }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-        ({loadState = StateModel
-        , login =
-          { email = ""
-          , password = ""
-          }
-        , singup =
-          { email = ""
-          , password = ""
-          }
-          ,emailState=0
-          ,pwdState=0
+        ({loadState = Loading
+        , form =
+            { email = ""
+            , password = ""
+            }
+        ,emailState=0
+        ,pwdState=0
         }
-        ,
-        Cmd.none
-        --  Http.post
-        --   { url = "/login"
-        --   , expect = Http.expectString GotText
-        --   }
+        ,Cmd.none
         )
 
 
@@ -61,93 +63,93 @@ type StateModel
   | Success
 
 type Msg
-    = GotText (Result Http.Error String)
-    | LoginSubmit                              --登录提交
-    | SingupSubmit Singup                           --注册提交
-    | CheckEmail String --验证邮箱
-    | CheckPwd String --验证密码
+    = LoginSubmit                            --登录提交
+    | SingupSubmit                            --注册提交
+    -- | NoExpect (Result Http.Error String)
+    | GotText (Result Http.Error String)
+    | NoNeeResult (Result Http.Error String)
+    | ChangeEmail String
+    | ChangePwd String
+
+    -- | CheckEmail String --验证邮箱
+    -- | CheckPwd String --验证密码
+    
 
 
-loginUser : String -> String -> Cmd Msg
-loginUser email pwd = 
+loginUser : Form-> Cmd Msg
+loginUser login=
     Http.post
         {
             url="/loginUser",
             body=
-                multipleBody
+                multipartBody
                     [
-                        stringPart "" email,
-                        stringPart "" pwd
+                        stringPart "" login.email,
+                        stringPart "" login.password
                     ]
-        , expect = Http.f
+        , expect = Http.expectString GotText
+        }
+
+registerUser : Form-> Cmd Msg
+registerUser singup= 
+    Http.post
+        {
+            url="/registerUser",
+            body=
+                multipartBody
+                    [
+                        stringPart "" singup.email,
+                        stringPart "" singup.password
+                    ]
+        , expect = Http.expectString GotText
+        }
+
+gohome:Cmd Msg
+gohome=
+    Http.get
+        {
+            url="/gohome",
+            expect=Http.expectString GotText
         }
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotText result ->
-          case result of
-            Ok fullText ->
+        LoginSubmit->
+            let login=model.form
+            in
+                (model,loginUser login)
+        SingupSubmit->
+            let login=model.form
+            in
+                (model,registerUser login)
+        GotText _ ->
+            (model,gohome)
+        NoNeeResult _ ->
+            (model,Cmd.none)
+        ChangeEmail str->
+            updateForm (\form -> { form | email = str }) model
+        ChangePwd str->
+            updateForm (\form -> { form | password = str }) model
+            
 
-            Err _ ->
-              (Fail,Cmd.none)
+        -- NoExpect _ ->
+        --     (model,Cmd.none)
+-- checkEmail : String -> Bool
+-- checkEmail email=email==/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
 
-        LoginSubmit ->
-           if Bool.not (String.isEmpty email) && Bool.not (String.isEmpty pwd) then
-           -- email && pwd both not empty
-                --check correct
-                if (checkEmail model.email) && (checkPwd model.pwd) then
-                    -- can post req
-                    ({model|emailState=1,pwdState=1},)
-                else
-                     if checkEmail email then
-                        ({model|emailState=1},Cmd.none)
-                    else
-                        ({model|emailState=2},Cmd.none)
-                     if checkPwd pwd then
-                        ({model|pwdState=1},Cmd.none)
-                    else
-                        ({model|pwdState=2},Cmd.none)    
-           else
-           -- check one is empty
-            if String.isEmpty email then
-                ({model|emailState=0},Cmd.none)
-            else
-                (model,Cmd.none)
-            if String.isEmpty pwd then
-               ({model|pwdState=0},Cmd.none)
-            else           
-                (model,Cmd.none)
+-- checkPwd : String -> Bool
+-- checkPwd pwd=pwd==/(a-zA-Z0-9){7,}/
 
-        CheckEmail email->
-            if String.isEmpty email then
-                ({model|emailState=0},Cmd.none)
-            else
-                if checkEmail email then
-                    ({model|emailState=1},Cmd.none)
-                else
-                    ({model|emailState=2},Cmd.none)
-        CheckPwd pwd->
-            if String.isEmpty pwd then
-                ({model|pwdState=0},Cmd.none)
-            else
-                if checkPwd pwd then
-                    ({model|pwdState=1},Cmd.none)
-                else
-                    ({model|pwdState=2},Cmd.none)
-
-checkEmail : String -> Bool
-checkEmail email=email==/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
-
-checkPwd : String -> Bool
-checkPwd pwd=pwd==/(a-zA-Z0-9){7,}/
-
+updateForm : (Form -> Form) -> Model -> ( Model, Cmd Msg )
+updateForm transform model =
+    ( { model | form = transform model.form }, Cmd.none )
 
 view : Model -> Html Msg
 view model =
   case model.loadState of
-    Loading ->
-    div
+    _ ->
+        div
         [ class "all" ]
         [ div
             [ class "logo" ]
@@ -238,7 +240,7 @@ view model =
                     [ class "inputBox" ]
                     [ input
                     -- TODO check email
-                        [ id "txtName1", placeholder "Email",onInput (CheckEmail email) ]
+                        [ id "txtName1", placeholder "Email" ,onInput ChangeEmail]
                         []
                     ]
                 , a
@@ -254,7 +256,7 @@ view model =
                     [ class "inputBox" ]
                     [ input
                     -- TODO check password
-                        [ id "txtPwd", placeholder "Password" ,onInput (CheckPwd pwd)]
+                        [ id "txtPwd", placeholder "Password",onInput ChangePwd ]
                         []
                     ]
                 , a
